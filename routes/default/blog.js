0a93932cab9be3677e90cb3d1c4cb299d43950d2
Reece Harris <reece.harris98@protonmail.com>
2023-01-25 01:41:30 +0000

    sa

--- routes/default/blog.js
+++ routes/default/blog.js
@@ -1,13 +1,31 @@
 /* eslint-disable new-cap */
 'use strict';
 
-require('dotenv').config();
-
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/blog', async (req, res, next) => {
-    res.renderMin('./blog/post');
+const authHeader = {
+    headers: {
+        'Authorization': 'Bearer ddbc58254e61078f381ff1d1bc50e2949b29e0a68ad6d7f4fea4b8027e5926d0e5edd19761768203430ba9476985a0fa84295aba27f2eeb8ffcaab35dcfce449b93047d3401bbd7be6eeb3db9d1706a08c3a874abb4c2667c44f682a892bb50221e92c708ca59cfdef037c17797507946ba144d1b753a74fca7d990d3a44d285'
+    }
+};
+
+router.get('/blog/:slug', async (req, res, next) => {
+
+    axios.get(`https://cms.reeceharris.net/api/slugify/slugs/article/${req.params.slug}?populate=*`, authHeader)
+    .then(response => {
+        if (response.data != null) {
+            console.log(response.data.data.attributes.banner)
+            res.renderMin('./blog/post', {post: response.data});
+        } else {
+            res.renderMin('./blog/notfound');
+        }
+    })
+    .catch(error => {
+        res.renderMin('./error/500', {error:error});
+    });
+
 });
 
 module.exports = router;
