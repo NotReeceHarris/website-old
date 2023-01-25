777bf84ef75140db1bb056bc97a5f3a6fc637f8b
Reece Harris <reece@codegalaxy.co.uk>
2023-01-25 16:55:41 +0000

    modified:   routes/default/blog.js

--- routes/default/blog.js
+++ routes/default/blog.js
@@ -65,7 +65,7 @@ router.get('/rss', async (req, res, next) => {
             response.data.data.forEach(element => {
                 const pubdate = new Date(element.attributes.createdAt);
                 const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-                items += `<item><guid>https://reeceharris.net/cms/${element.attributes.slug}</guid><title>${element.attributes.title}</title><link>https://reeceharris.net/cms/${element.attributes.slug}</link><pubDate>${weekday[pubdate.getDay()]}, ${pubdate.getDate()} ${pubdate.toLocaleString('en-US', { month: 'short' })} ${pubdate.getFullYear()} ${pubdate.getHours()}:${pubdate.getMinutes()}:${pubdate.getSeconds()} GMT</pubDate></item>`
+                items += `<item><guid>https://reeceharris.net/blog/${element.attributes.slug}</guid><title>${element.attributes.title}</title><link>https://reeceharris.net/cms/${element.attributes.slug}</link><pubDate>${weekday[pubdate.getDay()]}, ${pubdate.getDate()} ${pubdate.toLocaleString('en-US', { month: 'short' })} ${pubdate.getFullYear()} ${pubdate.getHours()}:${pubdate.getMinutes()}:${pubdate.getSeconds()} GMT</pubDate></item>`
             });
 
             res.set('Content-Type', 'text/xml');
