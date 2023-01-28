3f97638f8a237eab4af5dc9dce34788204cd6f8b
Reece Harris <reece.harris98@protonmail.com>
2023-01-28 21:20:29 +0000

    modified:   public/blog.js 	new file:   public/hug-art.svg 	new file:   public/linkedin.svg 	new file:   public/monocle-art.svg 	new file:   public/salute-art.svg 	new file:   public/share.svg 	new file:   public/thebird-art.svg 	new file:   public/twitter.svg 	modified:   views/blog/author.ejs 	modified:   views/blog/post.ejs 	modified:   views/landing/landing.ejs

--- public/blog.js
+++ public/blog.js
@@ -20,6 +20,14 @@ $( document ).ready(function() {
         $(this).append(`<div id="copy-code" class="border-green-400 hidden cursor-pointer absolute text-white top-3 right-4 p-2 rounded-md bg-[#363636]"><svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path><rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect></svg></div>`)
     })
 
+    $('#copy-link').click(function() {
+        var textArea = document.createElement("textarea");
+        textArea.value = window.location.href ;
+        textArea.select();
+        textArea.setSelectionRange(0, 99999);
+        navigator.clipboard.writeText(textArea.value);
+    })
+
     $('code').click('#copy-code', function() {
         var textArea = document.createElement("textarea");
         textArea.value = $(this).parent().text();
