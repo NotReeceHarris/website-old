71aa815bdb834625f5572725394c3472b8249c7d
Reece Harris <reece.harris98@protonmail.com>
2023-01-29 16:31:59 +0000

    modified:   routes/default/landing.js 	new file:   routes/default/seo.js

--- routes/default/landing.js
+++ routes/default/landing.js
@@ -9,10 +9,4 @@ router.get('/', async (req, res, next) => {
     res.renderMin('./landing/landing');
 });
 
-router.get('/robots.txt', async (req, res, next) => {
-    res.type('text/plain')
-    res.send("User-agent: *\nDisallow: /");
-});
-
-
 module.exports = router;
---
+++ routes/default/seo.js
@@ -0,0 +1,37 @@
+/* eslint-disable new-cap */
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+
+router.get('/robots.txt', async (req, res, next) => {
+    res.type('text/plain')
+    res.send("User-agent: *\nDisallow: /");
+});
+
+router.get('/sitemap.xml', async (req, res, next) => {
+    res.set('Content-Type', 'text/xml');
+    res.send(`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://reeceharris.net/</loc>
+            <lastmod>2023-01-29T16:12:20+03:00</lastmod>
+        </url>
+        <url>
+            <loc>https://reeceharris.net/blogs</loc>
+            <lastmod>2023-01-29T07:56:12+03:00</lastmod>
+        </url>
+        <url>
+            <loc>https://reeceharris.net/sandbox</loc>
+            <lastmod>2023-01-29T07:56:12+03:00</lastmod>
+        </url>
+        <url>
+            <loc>https://reeceharris.net/privacy-policy</loc>
+            <lastmod>2023-01-29T07:56:12+03:00</lastmod>
+        </url>
+    </urlset>
+    `);
+});
+
+
+module.exports = router;
