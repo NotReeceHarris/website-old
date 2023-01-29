58f8790aadfa8a2defacfc5b30f95e54ba4e170e
Reece Harris <reece.harris98@protonmail.com>
2023-01-29 16:25:09 +0000

    modified:   routes/default/landing.js 	modified:   views/blog/author.ejs 	modified:   views/blog/blogs.ejs 	modified:   views/blog/post.ejs 	modified:   views/blog/topic.ejs 	modified:   views/landing/landing.ejs

--- routes/default/landing.js
+++ routes/default/landing.js
@@ -9,5 +9,10 @@ router.get('/', async (req, res, next) => {
     res.renderMin('./landing/landing');
 });
 
+router.get('/robots.txt', async (req, res, next) => {
+    res.type('text/plain')
+    res.send("User-agent: *\nDisallow: /");
+});
+
 
 module.exports = router;
