94d5dcc2fa483d6b82dce1fd04125dc662d93a52
Reece Harris <reece@codegalaxy.co.uk>
2023-01-31 12:28:50 +0000

    modified:   public/cube.js 	modified:   routes/default/blog.js 	modified:   views/blog/post.ejs

--- routes/default/blog.js
+++ routes/default/blog.js
@@ -52,11 +52,17 @@ router.get('/blog/:slug', async (req, res, next) => {
             }
         })
         .catch(error => {
-            if (error.response.status == 404) {
-                res.renderMin('./error/404', {error:error});
+            console.log(error)
+            if (error != null) {
+                if (error.response.status == 404) {
+                    res.renderMin('./error/404', {error:error});
+                } else {
+                    res.renderMin('./error/500', {error:error});
+                }
             } else {
                 res.renderMin('./error/500', {error:error});
             }
+            
         });
     } else {
         res.redirect('/')
@@ -87,8 +93,12 @@ router.get('/author/:id', async (req, res, next) => {
             }
         })
         .catch(error => {
-            if (error.response.status == 404) {
-                res.renderMin('./error/404', {error:error});
+            if (error) {
+                if (error.response.status == 404) {
+                    res.renderMin('./error/404', {error:error});
+                } else {
+                    res.renderMin('./error/500', {error:error});
+                }
             } else {
                 res.renderMin('./error/500', {error:error});
             }
@@ -109,8 +119,12 @@ router.get('/topic/:topic', async (req, res, next) => {
                     }
                 })
                 .catch(error => {
-                    if (error.response.status == 404) {
-                        res.renderMin('./error/404', {error:error});
+                    if (error) {
+                        if (error.response.status == 404) {
+                            res.renderMin('./error/404', {error:error});
+                        } else {
+                            res.renderMin('./error/500', {error:error});
+                        }
                     } else {
                         res.renderMin('./error/500', {error:error});
                     }
@@ -120,8 +134,12 @@ router.get('/topic/:topic', async (req, res, next) => {
             }
         })
         .catch(error => {
-            if (error.response.status == 404) {
-                res.renderMin('./error/404', {error:error});
+            if (error) {
+                if (error.response.status == 404) {
+                    res.renderMin('./error/404', {error:error});
+                } else {
+                    res.renderMin('./error/500', {error:error});
+                }
             } else {
                 res.renderMin('./error/500', {error:error});
             }
@@ -183,7 +201,7 @@ router.get('/api/latest', async (req, res, next) => {
 });
 
 router.get('/rss', async (req, res, next) => {
-    axios.get(`https://cms.reeceharris.net/api/articles?fields=title,createdAt,slug&sort[0]=publishedAt:desc&pagination[limit]=10`, authHeader)
+    axios.get(`https://cms.reeceharris.net/api/articles?fields=title,createdAt,description,slug&sort[0]=publishedAt:desc&pagination[limit]=10&populate=banner,topics`, authHeader)
     .then(response => {
         if (response.data != null) {
             
@@ -191,16 +209,31 @@ router.get('/rss', async (req, res, next) => {
             response.data.data.forEach(element => {
                 const pubdate = new Date(element.attributes.createdAt);
                 const weekday = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
-                items += `<item><guid>https://reeceharris.net/blog/${element.attributes.slug}</guid><title>${element.attributes.title}</title><link>https://reeceharris.net/blog/${element.attributes.slug}</link><pubDate>${weekday[pubdate.getDay()]}, ${pubdate.getDate()} ${pubdate.toLocaleString('en-US', { month: 'short' })} ${pubdate.getFullYear()} ${pubdate.getHours()}:${pubdate.getMinutes()}:${pubdate.getSeconds()} GMT</pubDate></item>`
+                let category = ''
+                if (element.attributes.topics) {
+                    element.attributes.topics.data.forEach(topic => {
+                        category += `<category domain="https://reeceharris.net/topic/${topic.attributes.slug}">${topic.attributes.title}</category>`
+                    })
+                }
+                items += `<item>
+                <title>${element.attributes.title}</title>
+                <guid>https://reeceharris.net/blog/${element.attributes.slug}</guid>
+                <link>https://reeceharris.net/blog/${element.attributes.slug}</link>
+                <language>en-us</language>
+                ${category}
+                <description>${element.attributes.description}</description>
+                <pubDate>${weekday[pubdate.getDay()]}, ${pubdate.getDate()} ${pubdate.toLocaleString('en-US', { month: 'short' })} ${pubdate.getFullYear()} ${pubdate.getHours()}:${pubdate.getMinutes()}:${pubdate.getSeconds()} GMT</pubDate>
+                </item>`
             });
 
             res.set('Content-Type', 'text/xml');
-            res.send(`<rss version="2.0"><channel><title>( ͡° ͜ʖ ͡°)</title><description>Latest articles from the reeceharris.net blog</description><link>https://reeceharris.net</link><image><title>( ͡° ͜ʖ ͡°)</title><url>https://cms.reeceharris.net/uploads/site_logo_5edcfddd75.png</url><link>https://reeceharris.net</link></image>${items}</channel></rss>`)
+            res.send(`<rss version="2.0"><channel><title>( ͡° ͜ʖ ͡°) reeceharris.net</title><description>Latest articles from the reeceharris.net blog</description><link>https://reeceharris.net</link><image><title>( ͡° ͜ʖ ͡°)</title><url>https://cms.reeceharris.net/uploads/site_logo_5edcfddd75.png</url><link>https://reeceharris.net</link></image>${items}</channel></rss>`)
         } else {
             res.renderMin('./error/404');
         }
     })
     .catch(error => {
+        console.log(error)
         res.renderMin('./error/500', {error:error});
     });
 });
