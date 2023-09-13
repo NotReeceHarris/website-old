c01da95a3f1c6c4ea3ac5a528dd39b55e1367ee7
Reece Harris <reeceharris@email.com>
2023-09-13 16:56:45 +0100

    modified:   routes/portfolio/index.js 	new file:   views/portfolio/index.ejs

--- routes/portfolio/index.js
+++ routes/portfolio/index.js
@@ -3,9 +3,34 @@
 
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
+
+const authHeader = {
+	headers: {
+		Authorization: process.env.cmsToken,
+	},
+};
 
 router.get('/', async (req, res, _next) => {
-	res.render('./portfolio/comingsoon');
+	axios.get('https://cms.reeceharris.net/api/portfolios?fields=title,content,url&populate=screenshots,portfolio_tags', authHeader)
+		.then(portfolios => {
+			axios.get('https://cms.reeceharris.net/api/portfolio-tags?fields=name', authHeader)
+				.then(tags => {
+					if (portfolios.data !== null && tags.data !== null) {
+						return res.render('./portfolio/index', {portfolios: portfolios.data, tags: tags.data});
+					}
+
+					return res.render('./portfolio/index', {portfolios: []});
+				})
+				.catch(error => {
+					console.log(error)
+					res.render('./error/500', {error});
+				});
+		})
+		.catch(error => {
+			res.render('./error/500', {error});
+		});
 });
 
 module.exports = router;
