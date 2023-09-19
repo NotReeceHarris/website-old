f77340c690985d449456eba401fe55baa48c4ed2
Reece Harris <reeceharris@email.com>
2023-09-19 10:50:08 +0100

    Optimisation

--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 'use strict';
 
-const minify = require('express-minify');
-const cookieParser = require('cookie-parser');
-const serveStatic = require('serve-static');
-const compression = require('compression');
 const express = require('express');
-const path = require('path');
 const app = express();
 
-/* View engine set to ejs */
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', require('path').join(__dirname, 'views'));
 
-/* Cookie parser */
-app.use(cookieParser());
+app.use(require('cookie-parser')());
 app.use(express.urlencoded({extended: true}));
 
-/* Compression and minify to speed up load times */
-app.use(compression());
-app.use(minify({
+app.use(require('compression')());
+app.use(require('express-minify')({
 	cache: __dirname + '/cache',
 }));
 app.use(require('./minify'));
 
-/* Add routes to uri */
 app.use((req, res, next) => {
 	res.setHeader('X-Powered-By', '¯\\(º_o)/¯');
 	res.locals.url = req.protocol + '://' + req.hostname + req.path;
@@ -38,25 +29,32 @@ app.use((req, res, next) => {
 app.use('/', require('./routes/default.js'));
 app.use('/portfolio', require('./routes/portfolio.js'));
 app.use('/sandbox', require('./routes/sandbox.js'));
-app.use('/static', serveStatic('./public'));
+app.use('/static', require('serve-static')('./public'));
 
 app.use((req, res) => {
 	if (res.statusCode === 500) {
 		if (req.accepts('html')) {
-			return res.render('./error/500');
+			return res.status(500).render('./error/500');
 		}
 
 		if (req.accepts('json')) {
-			return res.json({error: 'Not found'});
+			return res.status(500).json({error: 'System error!'});
 		}
 
-		return res.type('txt').send('Not found');
+		return res.status(500).type('txt').send('System error!');
 	}
 
-	return res.render('./error/404');
+	if (req.accepts('html')) {
+		return res.status(404).render('./error/404');
+	}
+
+	if (req.accepts('json')) {
+		return res.status(404).json({error: 'Not found'});
+	}
+
+	return res.type('txt').send('Not found');
 });
 
-/* START */
 const port = process.argv.slice(2)[0] === 'dev' ? 8081 : 80;
 if (!module.parent) {
 	app.listen(port, () => {
